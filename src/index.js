702b17e532cfd7155aa3afa32fe43c14aabc52fe
Jan Klimo <jan.klimo@gmail.com>
2023-10-08 16:34:01 +0700

    Drop polyfills

--- src/index.js
+++ src/index.js
@@ -1,35 +1,37 @@
-import './polyfills';
-import React from 'react';
-import ImgComponent from './img';
-import BackgroundImgComponent from './background';
-import BlurHashComponent from './blurhash';
-import CloudimageProvider, { CloudimageContext } from './provider';
-
+import React from "react";
+import BackgroundImgComponent from "./background";
+import BlurHashComponent from "./blurhash";
+import ImgComponent from "./img";
+import CloudimageProvider, { CloudimageContext } from "./provider";
 
 const Img = (props = {}) => {
   return (
     <CloudimageContext.Consumer>
-      {(context = {}) => <ImgComponent {...props} config={context.config}/>}
+      {(context = {}) => <ImgComponent {...props} config={context.config} />}
     </CloudimageContext.Consumer>
-  )
-}
+  );
+};
 
 const BackgroundImg = (props = {}) => {
   return (
     <CloudimageContext.Consumer>
-      {(context = {}) => <BackgroundImgComponent {...props} config={context.config }/>}
+      {(context = {}) => (
+        <BackgroundImgComponent {...props} config={context.config} />
+      )}
     </CloudimageContext.Consumer>
-  )
-}
+  );
+};
 
 const BlurHash = (props = {}) => {
   return (
     <CloudimageContext.Consumer>
-      {(context = {}) => <BlurHashComponent {...props} config={context.config }/>}
+      {(context = {}) => (
+        <BlurHashComponent {...props} config={context.config} />
+      )}
     </CloudimageContext.Consumer>
-  )
-}
+  );
+};
 
 export default Img;
 
-export { CloudimageProvider, Img, BackgroundImg, BlurHash };
+export { BackgroundImg, BlurHash, CloudimageProvider, Img };
--- src/polyfills.js
+++
@@ -1,3 +0,0 @@
-import 'core-js/features/array/find';
-import 'core-js/es/math/trunc';
-import 'core-js/features/array/for-each';
